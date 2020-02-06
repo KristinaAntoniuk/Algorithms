bfecd707c237aed6ba4e4d2ebb44ba5c4ad6c1ad
Kristina Antoniuk <kristina_antoniuk@outlook.com>
2020-02-06 18:10:36 +0200

    Test graphic generation added.

--- Algorithms/Controllers/HomeController.cs
+++ Algorithms/Controllers/HomeController.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Algorithms.Models;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Web;
 
 namespace Algorithms.Controllers
 {
@@ -32,6 +37,58 @@ namespace Algorithms.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult DynamicConnectivity()
+        {
+            return View();
+        }
+
+        public ActionResult Image()
+        {
+            var result = new InlineImageResult(200, 50);
+            result.Graphics.DrawString("Hello, World!", new Font("Verdana", 20, FontStyle.Regular, GraphicsUnit.Pixel), new SolidBrush(Color.Blue), 0, 0);
+            return result;
+        }
+    }
+
+    public sealed class InlineImageResult : ActionResult
+    {
+        private readonly Image bmp;
+        public InlineImageResult(Int32 width, Int32 height)
+        {
+            //the PixelFormat argument is required if we want to have transparency
+            this.bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+
+            this.Graphics = Graphics.FromImage(this.bmp);
+            //for higher quality
+            this.Graphics.CompositingQuality = CompositingQuality.HighQuality;
+
+            this.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+
+            this.Graphics.InterpolationMode = InterpolationMode.High;
+
+            //make the background transparent
+            this.Graphics.Clear(Color.Transparent);
+        }
+
+        public Graphics Graphics { get; private set; }
+
+        public override void ExecuteResult(ActionContext context)
+        {
+            using (this.bmp)
+
+            using (this.Graphics)
+
+            using (var stream = new MemoryStream())
+            {
+                //PNG because of transparency
+                var format = ImageFormat.Png;
+                this.bmp.Save(stream, format);
+                var img = String.Format("<img src=\"data:image/{0};base64,{1}\"/>", format.ToString().ToLower(), Convert.ToBase64String(stream.ToArray()));
+                byte[] data = System.Text.Encoding.UTF8.GetBytes(img);
+                context.HttpContext.Response.Body.Write(data);
+            }
         }
     }
 }
---
+++ Algorithms/Models/DataModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Algorithms.Models
+{
+    public class DataModel
+    {
+        public string Input { get; set; }
+        public string Output { get; set; }
+    }
+}
